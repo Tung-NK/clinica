4035a9eadb737e824b8775f7d504a9a23fe149b8
Tung-NK <tungnguyenkhac222@gmail.com>
2024-10-15 16:18:08 +0700

    create project

---
+++ fontend/src/app/authentication/login/login.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/shared/auth/auth.service';
+import { routes } from 'src/app/shared/routes/routes';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent implements OnInit {
+  public routes = routes;
+  public passwordClass = false;
+
+  form = new FormGroup({
+    email: new FormControl('tungnguyenkhac222@gmail.com', [
+      Validators.required,
+      Validators.email,
+    ]),
+    password: new FormControl('12345678', [Validators.required]),
+  });
+
+  get f() {
+    return this.form.controls;
+  }
+
+  constructor(public auth: AuthService) { }
+  ngOnInit(): void {
+    if (localStorage.getItem('authenticated')) {
+      localStorage.removeItem('authenticated');
+    }
+  }
+
+  loginFormSubmit() {
+    if (this.form.valid) {
+      this.auth.login(this.form.value.email ? this.form.value.email : '', this.form.value.password ? this.form.value.password : '')
+        .subscribe((resp: any) => {
+          console.log(resp);
+        }, error => {
+          console.log(error);
+        });
+    }
+  }
+  togglePassword() {
+    this.passwordClass = !this.passwordClass;
+  }
+}
